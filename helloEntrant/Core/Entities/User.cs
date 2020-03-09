271ed328c8902c9a2eec8c8cc3fd1287dbc8d25d
Andriy Haliv <haliv.andriy99@gmail.com>
2020-03-09 22:23:09 +0200

    Added DB Contex and Migrated Databse

--- helloEntrant/Core/Entities/User.cs
+++ helloEntrant/Core/Entities/User.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Core.Entities
 {
-    class User
+    public class User : IdentityUser
     {
+
     }
 }
