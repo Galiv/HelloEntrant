ec327e430797f8b3ad8fa50ee8e90068dce256f7
Diana Kuzmenko <diana9142@gmail.com>
2020-04-06 15:47:19 +0300

    Basic form for files uploading

--- helloEntrant/HelloEntrantServer/Controllers/UserController.cs
+++ helloEntrant/HelloEntrantServer/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HelloEntrantServer.Controllers
@@ -14,5 +16,31 @@ namespace HelloEntrantServer.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult PersonalCabinet()
+        {
+            return View();
+        }
+
+        /*[HttpPost]
+        public async Task<IActionResult> AddFile(IFormFileCollection uploads)
+        {
+            foreach (var uploadedFile in uploads)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                /*using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+            }
+           // _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }*/
     }
 }
