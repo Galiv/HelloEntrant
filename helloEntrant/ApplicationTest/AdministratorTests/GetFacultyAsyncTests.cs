0f116cfb7f19f771a73a2f50ede13d06b7e8ab45
Andriy Haliv <haliv.andriy99@gmail.com>
2020-05-04 14:02:03 +0300

    Added Tests for Administrator Services

---
+++ helloEntrant/ApplicationTest/AdministratorTests/GetFacultyAsyncTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core;
+using Core.Entities;
+using Infrastructure.Services;
+using Moq;
+using Xunit;
+
+namespace ApplicationTest.AdministratorTests
+{
+    public class GetFacultyAsyncTests
+    {
+        [Fact]
+        public async void GetFacultyAsync_ShouldReturnValidValues()
+        {
+            //arrange
+
+            University university = new University()
+            {
+                UniversityId = 1,
+                Name = "LNU",
+                UserId = "user"
+            };
+
+            Faculty faculty = new Faculty()
+            {
+                FacultyId = 1,
+                Name = "Fac1",
+                UniversityId = 1
+
+            };
+
+            List<Speciality> specialities = new List<Speciality>
+            {
+                new Speciality{Name = "s1", FacultyId = 1},
+                new Speciality{Name = "s2", FacultyId = 1}
+            };
+
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var mockUnitOfWork = fixture.Freeze<Mock<IUnitOfWork>>();
+
+
+            mockUnitOfWork.Setup(u => u.FacultyRepository.GetAsync(faculty.FacultyId)).ReturnsAsync(faculty);
+            mockUnitOfWork.Setup(f => f.SpecialityRepository.GetAllSpecialitiesWithFacultyId(faculty.FacultyId)).ReturnsAsync(specialities);
+            mockUnitOfWork.Setup(u => u.UniversityRepository.GetUniversityWithId(university.UniversityId)).ReturnsAsync(university);
+
+            var administratorService = fixture.Create<AdministratorService>();
+
+
+            //Act
+            var result = await administratorService.GetFacultyAsync(faculty.FacultyId);
+
+            //Assert
+
+            Assert.True(result.CurrentFaculty == faculty);
+        }
+        
+    }
+}
---
+++ helloEntrant/ApplicationTest/AdministratorTests/GetUniversityAsyncTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.DTOs.Administrator;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core;
+using Core.Entities;
+using Infrastructure.Services;
+using Moq;
+using Xunit;
+
+namespace ApplicationTest.AdministratorTests
+{
+    public class GetUniversityAsyncTests
+    {
+        [Fact]
+
+        public async void GetUniversityAsync_ShouldReturnValidValues()
+        {
+            //arrange
+            University university = new University()
+            {
+                UniversityId = 1,
+                Name = "LNU"
+            };
+
+            List<Faculty> faculties = new List<Faculty>
+            {
+                new Faculty{Name = "Fac1", UniversityId = 1},
+                new Faculty{Name = "Fac2", UniversityId = 1}
+            };
+
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var mockUnitOfWork = fixture.Freeze<Mock<IUnitOfWork>>();
+            
+
+            mockUnitOfWork.Setup(u => u.UniversityRepository.GetAsync(university.UniversityId)).ReturnsAsync(university);
+
+            mockUnitOfWork.Setup(f => f.FacultyRepository.GetAllFacultiesWithUniversityId(university.UniversityId)).ReturnsAsync(faculties);
+                       
+            var administratorService = fixture.Create<AdministratorService>();
+            //Act
+            var result = await administratorService.GetUniversityAsync(university.UniversityId);
+
+            //Assert
+
+            Assert.True(result.CurrentUniversity.Name == "LNU");
+        }
+
+    }
+}
